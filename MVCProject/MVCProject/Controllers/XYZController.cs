aa7358266396ea33116b0ae5e34bc0d7ed49f4c2
n_whjang <n_whjang@naver.com>
2017-12-12 20:51:28 +0900

    세번째 프로젝트 작업내

---
+++ MVCProject/MVCProject/Controllers/XYZController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class XYZController : Controller
+    {
+        // GET: XYZ
+        public ActionResult Index()
+        {
+            return View();
+
+
+
+        }
+    }
+}
